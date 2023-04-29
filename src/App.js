90e1997c03fb48d57de89152f4979a0ad4816148
Mithileshkumaar <mithilesh.kumaar@yahoo.com>
2023-04-29 09:42:25 +0530

    frontend updated

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import styled from '@emotion/styled';
+import Home from './components/Home';
+import './components/style.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
+
+<style>
+</style>
---
+++ src/components/Home.jsx
@@ -0,0 +1,65 @@
+
+import { TextField, Card } from '@mui/material/';
+import './style.css';
+
+export default function Home() {
+      return (
+            <div style={{
+
+
+            }}>
+
+
+
+                  <div class="login-box">
+                        <p>CHECK THE URL HERE</p>
+                        <form>
+                              <div class="user-box">
+                                    <input required="" name="" type="text" />
+                                    <label>Email</label>
+                              </div>
+                              <div class="user-box">
+                                    <input required="" name="" type="text" />
+                                    <label>Password</label>
+                              </div>
+                              <div class="user-box">
+                                    <input required="" name="" type="text" />
+                                    <label>Password</label>
+                              </div>
+                              <div class="user-box">
+                                    <input required="" name="" type="text" />
+                                    <label>Password</label>
+                              </div>
+                              <div class="user-box">
+                                    <input required="" name="" type="text" />
+                                    <label>Password</label>
+                              </div>
+                              <div class="user-box">
+                                    <input required="" name="" type="text" />
+                                    <label>Password</label>
+                              </div>
+                              <div class="user-box">
+                                    <input required="" name="" type="text" />
+                                    <label>Password</label>
+                              </div>
+                              <a href="#">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    Submit
+                              </a>
+                        </form>
+                  </div>
+
+
+
+
+            </div>
+      )
+
+}
+
+<style>
+
+</style>
